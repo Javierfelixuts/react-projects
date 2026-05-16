4fb464045efbe5c17506df1ea47dfe26c937980d
unknown <felixtorresjavierantonio@gmail.com>
2026-05-16 15:42:16 -0700

    finish 4 project accordion

--- src/4-accordion/components/Accordion.tsx
+++ src/4-accordion/components/Accordion.tsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
+import { FaMinus, FaPlus } from "react-icons/fa6";
+
 type AccordionProps = {
-    question: string,
-    info: string
-}
+  question: string;
+  info: string;
+  isOpen: boolean;
+  id: number;
+};
 
-const Accordion = ({question, info} : AccordionProps) => {
+const Accordion = ({ question, info }: AccordionProps) => {
+  const [isOpen, setIsOpen ] = useState(false);
+
+  const handleAccordion = () => {
+    console.log("isOpen", isOpen);
+    setIsOpen(!isOpen);
+  };
   return (
-    <div className="accordion shadow p-2 my-3 block">
-        <h3 className="text-lg font-bold">{question}</h3>
-        <p className="font-semibold">{info}</p>
+    <div className="accordion border border-slate-300 shadow rounded p-4 my-3 block w-full">
+      <div className="w-full flex justify-between items-start">
+        <h3 className="text-left text-lg font-bold inline">{question}</h3>
+        <button
+          className="m-2 p-2 -mt-1.5 text-red-400 rounded-full font-bold bg-gray-200 relative content-end"
+          onClick={handleAccordion}
+        >
+          {isOpen ? <FaMinus /> : < FaPlus />}
+        </button>
+      </div>
+      <p
+        className={` info font-semibold overflow-hidden transition ease-linear ${isOpen ? "max-h-40" : "max-h-0"}  `}
+      >
+        {info}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Accordion
+export default Accordion;
--- src/4-accordion/index.tsx
+++ src/4-accordion/index.tsx
@@ -2,14 +2,16 @@ import Accordion from "./components/Accordion"
 import "./style.css"
 import questions from "./data.ts"
 
+
 const AccordionApp = () => {
+  
   return (
     <main id="AccordionApp" className="flex justify-center items-center">
-        <div className="card flex">
-            <h2 className="text-3xl w-5/12 font-bold">Questions And Answers about the login</h2>
-            <div className="accordion-container w-8/12 p-4">
+        <div className="card flex flex-wrap">
+            <h2 className="text-left text-3xl w-4/12  font-bold">Questions And Answers about the login</h2>
+            <div className="accordion-container w-fit md:w-8/12">
             {questions.map((q) => 
-                <Accordion key={q.id} question={q.title} info={q.info}/>
+                <Accordion key={q.id} question={q.title} info={q.info} id={q.id} isOpen={true}/>
             )}
             </div>
         </div>
