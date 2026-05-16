99bf5e9653a9de18f2df5f5b9cdf85aae57ac079
unknown <felixtorresjavierantonio@gmail.com>
2026-05-15 21:04:27 -0700

    Init 4 project - accordion

---
+++ src/4-accordion/index.tsx
@@ -0,0 +1,16 @@
+import "./style.css"
+
+const AccordionApp = () => {
+  return (
+    <main id="AccordionApp" className="flex justify-center items-center">
+        <div className="card">
+            <h2 className="text-4xl">Questions And Answers about the login</h2>
+            <div className="accordion-container">
+
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default AccordionApp
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 /* import BirthdayReminderApp from './1-birthday-reminder' */
 /* import { ToursApp } from './2-tours' */
-import ReviewsApp from './3-reviews'
+/* import ReviewsApp from './3-reviews' */
+import AccordionApp from './4-accordion'
 import './App.css'
 function App() {
   return (
     <>
       {/* <BirthdayReminderApp /> */}
      {/*  <ToursApp /> */}
-      <ReviewsApp />
+      {/* <ReviewsApp /> */}
+      <AccordionApp />
     </>
   )
 }
