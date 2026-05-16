aec5c5b97c547a35925ecedd9f12d31ce5f32d81
unknown <felixtorresjavierantonio@gmail.com>
2026-05-15 22:25:43 -0700

    updated

--- src/4-accordion/components/Accordion.tsx
+++ src/4-accordion/components/Accordion.tsx
@@ -1,6 +1,14 @@
-const Accordion = () => {
+type AccordionProps = {
+    question: string,
+    info: string
+}
+
+const Accordion = ({question, info} : AccordionProps) => {
   return (
-    <div>Accordion</div>
+    <div className="accordion shadow p-2 my-3 block">
+        <h3 className="text-lg font-bold">{question}</h3>
+        <p className="font-semibold">{info}</p>
+    </div>
   )
 }
 
--- src/4-accordion/index.tsx
+++ src/4-accordion/index.tsx
@@ -1,12 +1,16 @@
+import Accordion from "./components/Accordion"
 import "./style.css"
+import questions from "./data.ts"
 
 const AccordionApp = () => {
   return (
     <main id="AccordionApp" className="flex justify-center items-center">
-        <div className="card">
-            <h2 className="text-4xl">Questions And Answers about the login</h2>
-            <div className="accordion-container">
-
+        <div className="card flex">
+            <h2 className="text-3xl w-5/12 font-bold">Questions And Answers about the login</h2>
+            <div className="accordion-container w-8/12 p-4">
+            {questions.map((q) => 
+                <Accordion key={q.id} question={q.title} info={q.info}/>
+            )}
             </div>
         </div>
     </main>
